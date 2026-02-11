fdf5430edbd8cc09a69f9acdc8ec7a0f32376760
JamieByers <jamiebyers10@icloud.com>
2026-02-11 23:19:52 +0000

    not working :/

--- lua/InheritanceTaxHaven/init.lua
+++ lua/InheritanceTaxHaven/init.lua
@@ -1,22 +1,20 @@
+print("ITH plugin loaded")
+
 local M = {}
 
 function M.getLines()
     local lines = {}
 
-    for buf, _ in pairs(vim.api.nvim_list_bufs()) do
-         if vim.api.nvim_buf_is_loaded(buf) then
+    for _, buf in ipairs(vim.api.nvim_list_bufs()) do
+        if vim.api.nvim_buf_is_loaded(buf) then
             for _, line in ipairs(vim.api.nvim_buf_get_lines(buf, 0, -1, false)) do
                 table.insert(lines, line)
             end
         end
     end
 
-
     vim.print(lines)
-
 end
 
-vim.api.nvim_create_user_command("ITHGetLines",
-    M.getLines,
-    {}
-)
+vim.api.nvim_create_user_command("ITHGetLines", M.getLines, {})
+
