aa526cac5acd22d699642adbca97337351fb9d7a
JamieByers <jamiebyers10@icloud.com>
2026-02-11 23:53:33 +0000

    setting up for parsing

--- lua/InheritanceTaxHaven/init.lua
+++ lua/InheritanceTaxHaven/init.lua
@@ -2,19 +2,21 @@ print("ITH plugin loaded")
 
 local M = {}
 
-function M.getLines()
-    local lines = {}
+function M.GetLines()
+    local buf_lines = {}
 
-    for _, buf in ipairs(vim.api.nvim_list_bufs()) do
+    for i, buf in ipairs(vim.api.nvim_list_bufs()) do
         if vim.api.nvim_buf_is_loaded(buf) then
-            for _, line in ipairs(vim.api.nvim_buf_get_lines(buf, 0, -1, false)) do
-                table.insert(lines, line)
+            local lines = {}
+            for j, line in ipairs(vim.api.nvim_buf_get_lines(buf, 0, -1, false)) do
+                table.insert(lines, {j, line})
             end
+            table.insert(buf_lines, {i, lines})
         end
     end
 
-    vim.print(lines)
+    vim.print(buf_lines)
 end
 
-vim.api.nvim_create_user_command("ITHGetLines", M.getLines, {})
+vim.api.nvim_create_user_command("ITHGetLines", M.GetLines, {})
 
